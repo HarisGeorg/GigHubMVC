7024e77f24977052879e46288ee221ba1bd0d958
Charalampos Georgakopoulos <xarisgeorge83@gmail.com>
2022-09-07 19:08:28 +0300

    Added Following Entity

--- GigHubMVC/Models/IdentityModels.cs
+++ GigHubMVC/Models/IdentityModels.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
@@ -13,6 +15,11 @@ namespace GigHubMVC.Models
         [Required]
         [StringLength(100)]
         public string Name { get; set; }
+
+        public ICollection<Following> Followers { get; set; }
+        public ICollection<Following> Followees { get; set; }
+
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
@@ -26,7 +33,8 @@ namespace GigHubMVC.Models
     {
         public DbSet<Gig> Gigs { get; set; }
         public DbSet<Genre> Genres { get; set; }
-        public DbSet<Attendance> Attendances { get; set; } 
+        public DbSet<Attendance> Attendances { get; set; }
+        public DbSet<Following> Followings { get; set; }
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
@@ -44,12 +52,21 @@ namespace GigHubMVC.Models
                 .WithMany()    //attendances, alla epeidi den exo to navigation property sto gig to grafo sketo. Etsi apeikonizo to Attendance <-> Gig
                 .WillCascadeOnDelete(false);
 
-
             // An egrafa edo 
             //modelBuilder.Entity<Attendance>()
             //    .HasRequired(a => a.Attendee)
             //    .WithMany()
             // O sindiasmos me to pano tha arkouse gia na dimiourgiso ManyToMany kai na antikatastiso ta Annotations telika
+
+            modelBuilder.Entity<ApplicationUser>()  //O xristis
+                .HasMany(u => u.Followers)          //mporei na exei pollous Followers
+                .WithRequired(f => f.Followee)      //o kathenas Follower m enan apaitoumeno Followee
+                .WillCascadeOnDelete(false);        //kai to CascadeOnDelete tha einai false
+
+            modelBuilder.Entity<ApplicationUser>()
+                .HasMany(u => u.Followees)
+                .WithRequired(f => f.Follower)
+                .WillCascadeOnDelete(false);
 
             base.OnModelCreating(modelBuilder);
         }
